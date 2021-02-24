102f26b25fac4fe71ba518298c63692b1d17a696
root <root@LAPTOP-17T58IJE.localdomain>
2021-02-23 22:10:03 -0500

    Proyect Vue Es

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzy Exchange</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de cryptomonedas más importantes a
+      través de una API Rest de CoinCap, <br />
+      Este proyecto es utilizado en el curso Vue.js Básico de Platzy
+    </p>
+    <router-link to="/" class="mt-5 trxt-xl text-green-600 hover:underline">
+      Volver al home</router-link
+    >
+  </div>
+</template>
---
+++ src/views/Error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl uppercase">Error</h1>
+
+    <router-link to="/" class="mt-5 trxt-xl text-green-600 hover:underline">
+      Volver al home</router-link
+    >
+  </div>
+</template>
